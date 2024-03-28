108445f85c4a5fdf7430df79d99d4445d6226c22
AbhishekASLK <bhoslea925@gmail.com>
2024-03-28 12:32:20 +0530

    configure the app for handle the scroll feature and image loading error

--- lib/news_app.dart
+++ lib/news_app.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+const double _kSize = 70;
 
 class NewsApp extends StatefulWidget {
   const NewsApp({super.key});
@@ -11,13 +14,11 @@ class NewsApp extends StatefulWidget {
 }
 
 class _NewsAppState extends State<NewsApp> {
-  int index = 1;
-  String imageUrl = '';
-  String heading = '';
-  String description = '';
-  String readMore = '';
+  int cindex = 0;
+  dynamic data;
+  bool isDataFetched = false;
+  late PageController pageController;
 
-  get data => null;
   @override
   void initState() {
     super.initState();
@@ -25,7 +26,7 @@ class _NewsAppState extends State<NewsApp> {
   }
 
   _launchURLApp() async {
-    var url = Uri.parse(readMore);
+    var url = Uri.parse(data['articles'][cindex]['url']);
     if (await canLaunchUrl(url)) {
       await launchUrl(url);
     } else {
@@ -40,11 +41,8 @@ class _NewsAppState extends State<NewsApp> {
       ),
     );
     setState(() {
-      final data = jsonDecode(response.body);
-      imageUrl = data['articles'][index]['urlToImage'];
-      heading = data['articles'][index]['title'];
-      description = data['articles'][index]['description'];
-      readMore = data['articles'][index]['url'];
+      data = jsonDecode(response.body);
+      isDataFetched = true;
     });
   }
 
@@ -52,54 +50,77 @@ class _NewsAppState extends State<NewsApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.9),
-      body: SafeArea(
-        child: Column(
-          children: [
-            ClipRRect(
-              child: Image.network(
-                imageUrl,
+      body: (!isDataFetched)
+          ? Center(
+              child: LoadingAnimationWidget.flickr(
+                leftDotColor: const Color(0xFF0063DC),
+                rightDotColor: const Color(0xFFFF0084),
+                size: _kSize,
               ),
+            )
+          : PageView.builder(
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                cindex = index;
+                return SafeArea(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        child: (data['articles'][index]['urlToImage'] == '')
+                            ? const SizedBox(
+                                height: 100,
+                                width: 100,
+                              )
+                            : Image.network(
+                                data['articles'][index]['urlToImage'],
+                                errorBuilder: (BuildContext context,
+                                    Object error, StackTrace? stackTrace) {
+                                  return const Column(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 20),
+                                        child: Center(
+                                          child: Icon(
+                                            Icons.error_outline,
+                                            color: Colors.red,
+                                            size: 48.0,
+                                          ),
+                                        ),
+                                      ),
+                                      Text('Unable to load image')
+                                    ],
+                                  );
+                                },
+                              ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              data['articles'][index]['title'],
+                              style: const TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              data['articles'][index]['description'],
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(
-                children: [
-                  Text(
-                    heading,
-                    style: const TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    description,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            index++;
-            getNews();
-          });
-        },
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
-        child: const Icon(
-          Icons.navigate_next,
-          size: 40,
-        ),
-      ),
       bottomNavigationBar: GestureDetector(
         onTap: _launchURLApp,
         child: Container(
@@ -110,25 +131,38 @@ class _NewsAppState extends State<NewsApp> {
           width: double.infinity,
           child: const Padding(
             padding: EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
               children: [
-                Text(
-                  'To read more',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w300,
-                  ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'To read more',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    Text(
+                      'Tap here',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  'Tap here',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w300,
-                  ),
+                Spacer(),
+                Icon(
+                  Icons.arrow_forward,
+                  color: Colors.yellow,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 20,
                 ),
               ],
             ),
